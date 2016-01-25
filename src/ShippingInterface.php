0759889cac111721c5660714e567067e31be5b2d
Andrew <browner12@gmail.com>
2016-01-24 21:39:47 -0600

    lots of work on the interface
    - remove all dependencies on Easypost.
    - utilize new Contracts
    - expand on address create to be more explicit in the parameters we
    expect
    - and length, width, and height to package parameters
    - new customs info and customs item methods for international shipping

--- src/ShippingInterface.php
+++ src/ShippingInterface.php
@@ -1,37 +1,73 @@
 <?php namespace browner12\shipping;
 
-use EasyPost\Address;
-use EasyPost\Parcel;
+use browner12\shipping\Contracts\Order;
+use browner12\shipping\Contracts\OrderLine;
 
 interface ShippingInterface
 {
     /**
      * create a shipment
      *
-     * @param \EasyPost\Address $to
-     * @param \EasyPost\Parcel  $parcel
-     * @return
+     * @param mixed $to
+     * @param mixed $from
+     * @param mixed $parcel
+     * @param mixed $customs
+     * @return mixed
      */
-    public function shipment(Address $to, Parcel $parcel);
+    public function shipment($to, $from, $parcel, $customs);
 
     /**
      * create an address
      *
-     * @param array $input
+     * @param string $name
+     * @param string $street
+     * @param string $street2
+     * @param string $city
+     * @param string $state
+     * @param string $zip
+     * @param string $country
      * @return mixed
      */
-    public function address(array $input);
+    public function address($name, $street, $street2, $city, $state, $zip, $country);
 
     /**
      * create a parcel
      *
      * @param float $weight
+     * @param float $length
+     * @param float $width
+     * @param float $height
      * @return mixed
      */
-    public function parcel($weight);
+    public function parcel($weight, $length, $width, $height);
 
     /**
-     * purchase
+     * create a customs info
+     *
+     * @param \browner12\shipping\Contracts\Order $order
+     * @return mixed
+     */
+    public function customsInfo(Order $order);
+
+    /**
+     * create a customs item
+     *
+     * @param \browner12\shipping\Contracts\OrderLine $orderLine
+     * @return mixed
+     */
+    public function customsItem(OrderLine $orderLine);
+
+    /**
+     * determine if the shipment requires an Exception and Exclusion Legend (EEL)
+     * or a Proof of Filing Citation (PFC) and return the appropriate value
+     *
+     * @param \browner12\shipping\Contracts\Order $order
+     * @return string
+     */
+    public function eecPfc(Order $order);
+
+    /**
+     * purchase a shipment
      *
      * @param string $shipmentId
      * @param string $rateId
