50e01b4004f9b157bb06e1ff40fb658ba3a03e7a
Andrew <browner12@gmail.com>
2016-01-24 21:46:43 -0600

    update EasypostShipping
    - fix namespaces
    - better error messages
    - new international shipping methods
    - utilize new contracts
    - adjust based on interface changes

--- src/EasypostShipping.php
+++ src/EasypostShipping.php
@@ -1,13 +1,16 @@
 <?php namespace browner12\shipping;
 
+use browner12\shipping\Contracts\Order;
+use browner12\shipping\Contracts\OrderLine;
 use EasyPost\Address;
+use EasyPost\CustomsInfo;
+use EasyPost\CustomsItem;
 use EasyPost\EasyPost;
 use EasyPost\Error as EasyPostException;
 use EasyPost\Parcel;
 use EasyPost\Rate;
 use EasyPost\Shipment;
 use EasyPost\Tracker;
-use Exception;
 
 class EasyPostShipping implements ShippingInterface
 {
@@ -23,32 +26,27 @@ class EasyPostShipping implements ShippingInterface
     /**
      * create a shipment
      *
-     * @param \EasyPost\Address $to
-     * @param \EasyPost\Parcel  $parcel
+     * @param mixed $to
+     * @param mixed $from
+     * @param mixed $parcel
+     * @param mixed $customs
      * @return \EasyPost\Shipment
-     * @throws \App\Shipping\ShippingException
+     * @throws \browner12\shipping\ShippingException
      */
-    public function shipment(Address $to, Parcel $parcel)
+    public function shipment($to, $from, $parcel, $customs)
     {
-        //from address
-        $from = $this->fromAddress();
-
         //shipment
         try {
             $shipment = Shipment::create([
-                "to_address"   => $to,
-                "from_address" => $from,
-                "parcel"       => $parcel,
+                'to_address'   => $to,
+                'from_address' => $from,
+                'parcel'       => $parcel,
+                'customs'      => $customs,
             ]);
         }
 
             //easypost exception
         catch (EasyPostException $e) {
-            throw new ShippingException('Unable to create shipment.', 0, $e);
-        }
-
-            //exception
-        catch (Exception $e) {
             throw new ShippingException('Unable to create shipment.', 0, $e);
         }
 
@@ -57,31 +55,36 @@ class EasyPostShipping implements ShippingInterface
     }
 
     /**
-     * address
+     * create an address
      *
-     * @param array $input
+     * @param string $name
+     * @param string $street
+     * @param string $street2
+     * @param string $city
+     * @param string $state
+     * @param string $zip
+     * @param string $country
      * @return \EasyPost\Address
-     * @throws \App\Shipping\ShippingException
+     * @throws \browner12\shipping\ShippingException
      */
-    public function address(array $input)
+    public function address($name, $street, $street2, $city, $state, $zip, $country)
     {
-        //address
-        $inputAddress['name'] = (isset($input['name'])) ? $input['name'] : null;
-        $inputAddress['street1'] = (isset($input['street'])) ? $input['street'] : null;
-        $inputAddress['street2'] = (isset($input['street2'])) ? $input['street2'] : null;
-        $inputAddress['city'] = (isset($input['city'])) ? $input['city'] : null;
-        $inputAddress['state'] = (isset($input['state'])) ? $input['state'] : null;
-        $inputAddress['zip'] = (isset($input['zip'])) ? $input['zip'] : null;
-        $inputAddress['country'] = (isset($input['country']) ? $input['country'] : null);
-
-        //try to create address object
+        //create address object
         try {
-            $address = Address::create($inputAddress);
+            $address = Address::create([
+                'name'    => $name,
+                'street'  => $street,
+                'street2' => $street2,
+                'city'    => $city,
+                'state'   => $state,
+                'zip'     => $zip,
+                'country' => $country,
+            ]);
         }
 
             //easypost exception
         catch (EasyPostException $e) {
-            throw new ShippingException('Easypost exception creating address.', 0, $e);
+            throw new ShippingException('Unable to create address.', 0, $e);
         }
 
         //return address object
@@ -89,65 +92,34 @@ class EasyPostShipping implements ShippingInterface
     }
 
     /**
-     * snb from address
-     *
-     * @return \EasyPost\Address
-     * @throws \App\Shipping\ShippingException
-     */
-    public function fromAddress()
-    {
-        //address
-        $fromAddress = [
-            'company' => config('snb.name'),
-            'street1' => config('snb.street'),
-            'street2' => config('snb.street2'),
-            'city'    => config('snb.city'),
-            'state'   => config('snb.state'),
-            'zip'     => config('snb.zip'),
-            'phone'   => config('snb.phone'),
-            'email'   => config('snb.orderEmail'),
-        ];
-
-        //try to create address object
-        try {
-            $address = Address::create($fromAddress);
-        }
-
-            //easypost exception
-        catch (EasyPostException $e) {
-            throw new ShippingException('Easypost exception creating from address.', 0, $e);
-        }
-
-        //return address object
-        return $address;
-    }
-
-    /**
-     * parcel
+     * create a parcel
      *
      * weight needs to be in ounces (oz)
      * length, width, and height need to be inches (in)
      *
      * @param float $weight
+     * @param float $length
+     * @param float $width
+     * @param float $height
      * @return \EasyPost\Parcel
-     * @throws \App\Shipping\ShippingException
+     * @throws \browner12\shipping\ShippingException
      */
-    public function parcel($weight)
+    public function parcel($weight, $length, $width, $height)
     {
         //try to create parcel
         try {
             $parcel = Parcel::create([
                 'predefined_package' => null,
                 'weight'             => round($weight, 1),
-                'length'             => null,
-                'width'              => null,
-                'height'             => null,
+                'length'             => round($length, 1),
+                'width'              => round($width, 1),
+                'height'             => round($height, 1),
             ]);
         }
 
             //easypost exception
         catch (EasyPostException $e) {
-            throw new ShippingException('Unable to create Easypost parcel.', 0, $e);
+            throw new ShippingException('Unable to create parcel.', 0, $e);
         }
 
         //return parcel object
@@ -155,12 +127,74 @@ class EasyPostShipping implements ShippingInterface
     }
 
     /**
-     * purchase shipment
+     * create a customs info
+     *
+     * @param \browner12\shipping\Contracts\Order $order
+     * @return mixed
+     */
+    public function customsInfo(Order $order)
+    {
+        //initialize customs items
+        $customsItems = [];
+
+        //loop through order lines
+        foreach ($order->getOrderLines() as $orderLine) {
+            $customsItems[] = $this->customsItem($orderLine);
+        }
+
+        //create and return customs info
+        return CustomsInfo::create([
+            'eel_pfc'         => 'NOEEI 30.37(a)',
+            'customs_certify' => true,
+            'customs_signer'  => config('SHIPPING_CUSTOMS_SIGNER'),
+            'contents_type'   => 'gift',
+            'customs_items'   => $customsItems,
+        ]);
+    }
+
+    /**
+     * create a customs item
+     *
+     * @param \browner12\shipping\Contracts\OrderLine $orderLine
+     * @return mixed
+     */
+    public function customsItem(OrderLine $orderLine)
+    {
+        return CustomsItem::create([
+            'description'      => $orderLine->getProduct()->getDescription(),
+            'quantity'         => $orderLine->getQuantity(),
+            'weight'           => $orderLine->getWeight(),
+            'value'            => $orderLine->getValue(),
+            'hs_tariff_number' => $orderLine->getProduct()->getTariffNumber(),
+            'origin_country'   => $orderLine->getProduct()->getOriginCountry(),
+        ]);
+    }
+
+    /**
+     * determine if the shipment requires an Exception and Exclusion Legend (EEL)
+     * or a Proof of Filing Citation (PFC) and return the appropriate value
+     *
+     * @param \browner12\shipping\Contracts\Order $order
+     * @return string
+     */
+    public function eecPfc(Order $order)
+    {
+        //value is over 2500 USD so we need a Internal Transaction Number
+        if ($order->getValue() > 250000) {
+            return config('SHIPPING_INTERNAL_TRANSACTION_NUMBER');
+        }
+
+        //value is less than 2500 USD
+        return 'NOEEI 30.37(a)';
+    }
+
+    /**
+     * purchase a shipment
      *
      * @param string $shipmentId
      * @param string $rateId
      * @return null
-     * @throws \App\Shipping\ShippingException
+     * @throws \browner12\shipping\ShippingException
      */
     public function purchase($shipmentId, $rateId)
     {
@@ -181,7 +215,7 @@ class EasyPostShipping implements ShippingInterface
         catch (EasypostException $e) {
 
             //throw shipping exception
-            throw new ShippingException('Problem purchasing label.' . $e->getMessage(), 0, $e);
+            throw new ShippingException('Unable to purchase label.' . $e->getMessage(), 0, $e);
         }
 
         //return
@@ -193,7 +227,7 @@ class EasyPostShipping implements ShippingInterface
      *
      * @param string $shipmentId
      * @return string
-     * @throws \App\Shipping\ShippingException
+     * @throws \browner12\shipping\ShippingException
      */
     public function label($shipmentId)
     {
@@ -213,7 +247,7 @@ class EasyPostShipping implements ShippingInterface
         catch (EasyPostException $e) {
 
             //throw shipping exception
-            throw new ShippingException('Problem retrieving label.', 0, $e);
+            throw new ShippingException('Unable to retrieve label.', 0, $e);
         }
     }
 
@@ -222,7 +256,7 @@ class EasyPostShipping implements ShippingInterface
      *
      * @param string $trackingId
      * @return \EasyPost\Tracker
-     * @throws \App\Shipping\ShippingException
+     * @throws \browner12\shipping\ShippingException
      */
     public function track($trackingId)
     {
@@ -237,7 +271,7 @@ class EasyPostShipping implements ShippingInterface
         catch (EasyPostException $e) {
 
             //throw shipping exception
-            throw new ShippingException('Problem tracking shipment.', 0, $e);
+            throw new ShippingException('Unable to track shipment.', 0, $e);
         }
     }
 
