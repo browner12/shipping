89e4917fb9f308ee95c4ce57d3483a7b44785d86
Andrew <browner12@gmail.com>
2016-01-27 00:34:45 -0600

    don't use environment variables when not appropriate
    some variables are not dependent on environments. i shouldn’t have been
    using them everywhere

--- src/config/shipping.php
+++ src/config/shipping.php
@@ -12,7 +12,19 @@ return [
     | Supported: 'easypost', 'postmaster', 'shippo'
     |
     */
-    'default' => env('SHIPPING_SERVICE'),
+
+    'default' => '',
+
+    /*
+    |--------------------------------------------------------------------------
+    | API Key
+    |--------------------------------------------------------------------------
+    |
+    | Set the API key of your selected shipping service.
+    |
+    */
+
+    'api_key' => env('YOUR_API_KEY'),
 
     /*
     |--------------------------------------------------------------------------
@@ -23,61 +35,63 @@ return [
     |
     */
 
-    'from_street' => env('SHIPPING_FROM_STREET'),
-    'from_city'   => env('SHIPPING_FROM_CITY'),
-    'from_state'  => env('SHIPPING_FROM_STATE'),
-    'from_zip'    => env('SHIPPING_FROM_ZIP'),
+    'from_street' => '',
+    'from_city'   => '',
+    'from_state'  => '',
+    'from_zip'    => '',
 
     /*
     |--------------------------------------------------------------------------
     | Contents Type
     |--------------------------------------------------------------------------
     |
-    | The type of item you are sending.
+    | The default type of item you are sending.
     |
     | Supported: 'merchandise', 'returned_goods', 'documents', 'gift', 'sample', 'other'
     |
     */
 
-    'contents_type' => env('SHIPPING_CONTENTS_TYPE', 'merchandise'),
+    'contents_type' => 'merchandise',
 
     /*
     |--------------------------------------------------------------------------
     | Customs Signer
     |--------------------------------------------------------------------------
     |
-    | This is the name of the person who is certifying that the information provided on the customs
-    | form is accurate. Use a name of the person in your organization who is responsible for this.
+    | This is the name of the person who is certifying that the information
+    | provided on the customs form is accurate. Use a name of the person
+    | in your organization who is responsible for this.
     |
     */
 
-    'customs_signer' => env('SHIPPING_CUSTOMS_SIGNER'),
+    'customs_signer' => '',
 
     /*
     |--------------------------------------------------------------------------
     | Non Delivery Option
     |--------------------------------------------------------------------------
     |
-    | In case the shipment cannot be delivered, this option tells the carrier what you want to
-    | happen to the package.  If you pass ‘abandon’, you will not receive the package back
-    | if it cannot be delivered.
+    | In case the shipment cannot be delivered, this option tells the carrier
+    | what you want to happen to the package.  If you pass ‘abandon’, you
+    | will not receive the package back if it cannot be delivered.
     |
     | Supported: 'return', 'abandon'
     |
     */
 
-    'non_delivery_option' => env('SHIPPING_NON_DELIVERY_OPTION', 'return'),
+    'non_delivery_option' => 'return',
 
     /*
     |--------------------------------------------------------------------------
     | Internal Transaction Number
     |--------------------------------------------------------------------------
     |
-    | An ITN is required for any international shipment valued over $2,500 and/or requires an export
-    | license unless exemptions apply. Visit http://aesdirect.census.gov/ to obtain one.
+    | An ITN is required for any international shipment valued over $2,500
+    | and/or requires an export license unless exemptions apply. Visit
+    | http://aesdirect.census.gov/ to obtain one.
     |
     */
 
-    'internal_transaction_number' => env('SHIPPING_INTERNAL_TRANSACTION_NUMBER'),
+    'internal_transaction_number' => '',
 
 ];
