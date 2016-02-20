dda32336ec4cbb4406a8ae84cfa9966a1bf95d17
Andrew <browner12@gmail.com>
2016-02-20 01:02:49 -0600

    update service provider
    make implementation configurable based on config file

--- src/ShippingServiceProvider.php
+++ src/ShippingServiceProvider.php
@@ -5,17 +5,54 @@ use Illuminate\Support\ServiceProvider;
 class ShippingServiceProvider extends ServiceProvider
 {
     /**
-     * register bindings
+     * register the service provider
+     *
+     * @return void
      */
     public function register()
     {
-        $this->app->bind('browner12\shipping\ShippingInterface', 'browner12\shipping\EasypostShipping');
+        $this->app->bind('browner12\shipping\ShippingInterface', $this->getShippingService());
     }
 
+    /**
+     * boot the service provider
+     *
+     * @return void
+     */
     public function boot()
     {
+        //publish config
         $this->publishes([
-            __DIR__.'/config/shipping.php' => config_path('shipping.php'),
+            __DIR__ . '/config/shipping.php' => config_path('shipping.php'),
         ]);
+    }
+
+    /**
+     * determine the shipping service to use
+     *
+     * @return string
+     */
+    private function getShippingService()
+    {
+        switch (config('shipping.default')) {
+
+            //easypost
+            case 'easypost':
+
+                return 'browner12/shipping/EasypostShipping';
+                break;
+
+            //shippo
+            case 'shippo':
+
+                return 'browner12/shipping/ShippoShipping';
+                break;
+
+            //default
+            default:
+
+                return '';
+                break;
+        }
     }
 }
